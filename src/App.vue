e4a4bb429583f59ff037dc58fa64c80c68bd0419
Reda Chehabi <chehabire.tool08@gmail.com>
2026-05-25 21:15:21 +0100

    page icon and analytics added

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
   import { RouterView } from 'vue-router'
   import NavBar from './components/NavBar.vue';
   import FooterSection from './components/FooterSection.vue';
+  import { Analytics } from "@vercel/analytics/vue"
 </script>
-
 <template>
   <div>
+    <Analytics/>
     <NavBar/>
     <RouterView />
     <FooterSection />
